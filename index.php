3f4c23eea155e5da683f2ae3dd0b3e7ba097d295
just-cmm503 <cmm503-Just>
2017-04-13 22:07:28 +0100

    first

--- index.php
+++ index.php
@@ -5,15 +5,31 @@
  * Date: 13/04/2017
  * Time: 13:06
  */
+include "dbConnect.php";
+
+////$sql = "select * from users where id = ?";
+////$usrs_v=2;
+// execute the SQL query
+//$result=mysqli_query($link,$sql);
+//$result=$link->query($sql);
+
+//$hRow=mysqli_fetch_fields($result);
+
+//$sql_insert = "INSERT INTO registration_tbl (name, email, date)                     VALUES (?,?,?)";
+////$stmt = $conn->prepare($sql);
+////$stmt->bindValue(1,$usrs_v);
+//$stmt->bindValue(2, $email);
+//$stmt->bindValue(3, $date);
+////$stmt->execute();
+//}
+//catch(Exception $e) {
+//    die(var_dump($e));
+//}
+
+$sql = "select * from users";
+
+$result=$link->query($sql);
+print json_encode($result);
+$result->close();
+$link->close();
 ?>
-<html>
-<head> 
-<title>Justice's Course Work</title>
-</head>
-
-<body>
-
-<?php print fetchRecord('users') + " ... \r\n "; ?>
-
-</body>
-</html>
