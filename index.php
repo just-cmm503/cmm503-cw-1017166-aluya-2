1959b3aca1108894dc971d95f8a801d949d63f54
just-cmm503 <cmm503-Just>
2017-04-13 21:27:54 +0100

    first

--- index.php
+++ index.php
@@ -5,5 +5,19 @@
  * Date: 13/04/2017
  * Time: 13:06
  */
+
+print "
+<html>
+<head> 
+<title>Justice's Course Work</title>
+</head>
+
+<body>
+";
 print fetchRecord('users');
+
+print "
+</body>
+</html>
+";
 ?>
