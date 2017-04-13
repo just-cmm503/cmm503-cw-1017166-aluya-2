4101eca9aae7ccef31e4f074bfad0c627f6ffc0b
just-cmm503 <cmm503-Just>
2017-04-13 21:17:38 +0100

    first

---
+++ fetchRecord.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1017166
+ * Date: 13/04/2017
+ * Time: 21:08
+ */
+
+include "dbConnect.php";
+
+function fetchRecord($tableName){
+    $sql = "select * from users";
+
+    $result = $link->query($sql);
+
+    while ($row = mysqli_fetch_assoc($result)) {
+    };
+    $fetchRecord_out = json_decode($result);
+    $result->close();
+    $link->close();
+    return $fetchRecord_out;
+}
+
+?>
