30da18d05994a18560dd4c336c168897cf9632d6
Justice aluya <justice015@gmail.com>
2017-04-15 05:44:30 +0100

    ok

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
  * Time: 13:06
  */
 require_once "dbConnect.php";
-$myJobj= fetchRecords('users',$link);
+$myJobj= json_decode(fetchRecords('users',$link));
 //print fetchRecords('users',$link);
-foreach($row as $myJobj){
-    print_r($row);
+foreach ($myJobj as $row ){
+    print($row). " ";
 }
 ?>
